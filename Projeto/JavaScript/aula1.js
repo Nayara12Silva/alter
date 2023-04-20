7f5ad55c0dfcdf14e8396f4d9af3041af57b5672
Nayara <104741196+Nayara12Silva@users.noreply.github.com>
2023-04-20 11:34:48 -0300

    javascript 20/04

--- Projeto/JavaScript/aula1.js
+++ Projeto/JavaScript/aula1.js
@@ -1,45 +1,25 @@
 // Variáveis
-const Total = 0
-const Vinho_Tinto = 0
-const Vinho_Branco = 0
-const Vinho_Rose = 0
+var Total = [];
+var Vinho_Tinto = 0
+var Vinho_Branco = 0
+var Vinho_Rose = 0
+//Manipulação
+var vinhos = document.getElementById('vinhos');
+var garrafa = document.getElementById('garrafa');
 
 function Estoque(){
-   const Vinhos = String(prompt('escolha o tipo de vinho: T - Vinho Tinto , B - Vinho Branco , R - Vinho Rosé , F - Finalizar '));
+var TipoSelecionado = vinhos.value;
 
-   while(Vinhos!=='F'){
-    Vinhos = String(prompt('escolha o tipo de vinho: T - Vinho Tinto , B - Vinho Branco , R - Vinho Rosé , F - Finalizar '));
-    const quantidade = Number(prompt('Digite a quantidade de garrafas:'));
-
-    if(Vinhos =='T'){
-        Vinho_Tinto = quantidade
+        if(TipoSelecionado=='Tinto'){
+            Vinho_Tinto = garrafa.value
+            console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Tinto} garrafas no estoque`);
+        }else if(TipoSelecionado=='Branco'){
+            Vinho_Branco = garrafa.value
+            console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Branco} garrafas no estoque`);
+        }else if(TipoSelecionado=='Rose'){
+            Vinho_Rose = garrafa.value
+            console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Rose} garrafas no estoque`);
+        }else{
+            console.log('Você finalizou a entrada de dados')
+        }
     }
-   }
-}
-Estoque()
-
-// function Escolha_Vinhos(){
-//     var escolha = (prompt(" T - Vinho Tinto , B - Vinho Branco , R - Vinho Rosé , F - Finalizar "));
-
-
-// switch (escolha){
-//          case ('T'): 
-//              Estoque()
-//              break;
-//         case "B" :
-            
-//             break;
-//         case "R" :
-            
-//             break;
-//          case "F" :
-//                     esc = alert ( "Fim da Entrada de Dados");
-//                     break;
-
-//         default:
-//             esc = alert("Opção Inexistente")
-//             Escolha_Vinhos()
-// }
-// }
-// Escolha_Vinhos()
-
