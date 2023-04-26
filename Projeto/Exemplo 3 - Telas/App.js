28280320b9f53ffd37ca1aee1ffcc37b9e53b6df
Nayara Silva <kuchiki604@gmail.com>
2023-04-26 06:46:10 -0300

    javascript 26/4

--- Projeto/Exemplo 3 - Telas/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
-
-import Servicos from './src/telas/Servicos';
-
-export default function App() {
-  return <Servicos />;
-}
--- Projeto/JavaScript/aula1.js
+++ Projeto/JavaScript/aula1.js
@@ -1,25 +1,41 @@
-// Variáveis
+//Manipulação 
 var Total = [];
-var Vinho_Tinto = 0
-var Vinho_Branco = 0
-var Vinho_Rose = 0
-//Manipulação
 var vinhos = document.getElementById('vinhos');
 var garrafa = document.getElementById('garrafa');
+var Tabela = document.getElementById('tabela');
 
 function Estoque(){
-var TipoSelecionado = vinhos.value;
-
+    var TipoSelecionado = vinhos.value;
         if(TipoSelecionado=='Tinto'){
-            Vinho_Tinto = garrafa.value
+            Vinho_Tinto = garrafa.value;
+            Total.push(Vinho_Tinto);
             console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Tinto} garrafas no estoque`);
+             
         }else if(TipoSelecionado=='Branco'){
-            Vinho_Branco = garrafa.value
+            Vinho_Branco = garrafa.value;
+            Total.push(Vinho_Branco);
             console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Branco} garrafas no estoque`);
+
         }else if(TipoSelecionado=='Rose'){
-            Vinho_Rose = garrafa.value
+            Vinho_Rose = garrafa.value;
+            Total.push(Vinho_Rose);
             console.log(`O tipo selecionado é ${TipoSelecionado} possui ${Vinho_Rose} garrafas no estoque`);
         }else{
             console.log('Você finalizou a entrada de dados')
         }
-    }
+
+}
+// function Porcentagem(){
+//     var Porcentagem_Vinho_Tinto = Vinho_Tinto/Total*100
+//     var Porcentagem_Vinho_Branco = Vinho_Branco/Total*100
+//     var Porcentagem_Vinho_Rose = Vinho_Rose/Total*100
+//     if(Total > 0){
+
+//     }
+// }
+
+
+// var EstoqueTotal = Total.reduce(function(soma, i) {
+//      return soma + i;
+//  });
+//  console.log(EstoqueTotal);
